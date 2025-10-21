c7cf3c18d06916333781cb9c9d7d02984376d2d5
przemek <przemek.bialkwno@gmail.com>
2025-10-21 18:37:54 +0200

    clearing default files, installing tailiwnd

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <>hello</>;
+  return (
+    <div className=" flex h-screen items-center justify-center bg-blue-500 text-white text-4xl font-bold">
+      Tailwind v4 działa 🚀
+    </div>
+  );
 }
 
 export default App;
