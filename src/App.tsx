4653796ceea8573ebbe5c03a8e037e70ff05726e
przemek <przemek.bialkwno@gmail.com>
2025-10-22 07:09:11 +0200

    creating .env, making first API call

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
+
 function App() {
-  return (
-    <div className=" flex h-screen items-center justify-center bg-blue-500 text-white text-4xl font-bold">
-      Tailwind v4 działa 🚀
-    </div>
-  );
+  useEffect(() => {
+    const fetchCurrentWeather = async (): Promise<void> => {
+      try {
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=metric&appid=${
+            import.meta.env.VITE_WEATHER_API_KEY
+          }`
+        );
+        const data = await response.json();
+        console.log("done", data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchCurrentWeather();
+  }, []);
+
+  return <div className=" bg-blue-900">Tailwind</div>;
 }
 
 export default App;
