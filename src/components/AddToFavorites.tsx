9e7cd9fd980b4fd45ac228ee27717b02ff657ced
przemek <przemek.bialkwno@gmail.com>
2025-11-04 16:15:38 +0100

    working on add to favorites component

--- src/components/AddToFavorites.tsx
+++ src/components/AddToFavorites.tsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import { useWeatherContext } from "../hooks/useWeatherContext";
 
-const AddToFavorites = () => {
-  type Favorite = { lat: number; lon: number };
-  type Favorites = Favorite[];
+type Favorite = { lat: number; lon: number };
+type Favorites = Favorite[];
 
+const AddToFavorites = () => {
+  const [message, setMessage] = useState<string | null>(null);
   const { city } = useWeatherContext();
 
   const lat = city?.coord.lat;
@@ -12,28 +14,37 @@ const AddToFavorites = () => {
   const handleClick = (lat?: number, lon?: number) => {
     if (lat === undefined || lon === undefined) return;
 
-    // get the list
     const stored = localStorage.getItem("favorites");
     const favorites: Favorites = stored ? JSON.parse(stored) : [];
 
-    // if already exists
     const exists = favorites.some((fav) => fav.lat === lat && fav.lon === lon);
-    if (exists) return;
 
-    favorites.push({ lat, lon });
+    if (exists) {
+      setMessage("❗already on your list!");
+    } else {
+      favorites.push({ lat, lon });
+      localStorage.setItem("favorites", JSON.stringify(favorites));
+      setMessage("✅ added");
+    }
 
-    localStorage.setItem("favorites", JSON.stringify(favorites));
-
-    console.log("Dodano:", { lat, lon });
+    setTimeout(() => setMessage(null), 1500);
   };
 
   return (
-    <button
-      onClick={() => handleClick(lat, lon)}
-      className="bg-blue-500 text-white p-2 rounded"
-    >
-      add to favorites
-    </button>
+    <>
+      <button
+        onClick={() => handleClick(lat, lon)}
+        className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition"
+      >
+        Add to favorites
+      </button>
+
+      {message && (
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black/80 text-white px-4 py-2 rounded-xl shadow-lg text-lg font-semibold z-50 ">
+          {message}
+        </div>
+      )}
+    </>
   );
 };
 
--- src/components/RemoveFromFavorites.tsx
+++ src/components/RemoveFromFavorites.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+
 type RemoveFromFavoritesProps = {
   lat: number;
   lon: number;
 };
 
 const RemoveFromFavorites = ({ lat, lon }: RemoveFromFavoritesProps) => {
+  const [message, setMessage] = useState<string | null>(null);
+
   const handleRemove = (lat: number, lon: number) => {
     console.log("usunięto:", lat, lon);
 
@@ -17,15 +21,24 @@ const RemoveFromFavorites = ({ lat, lon }: RemoveFromFavoritesProps) => {
     );
 
     localStorage.setItem("favorites", JSON.stringify(updatedList));
+    setMessage("usunieto");
+    setTimeout(() => setMessage(null), 1500);
   };
 
   return (
-    <button
-      onClick={() => handleRemove(lat, lon)}
-      className="bg-red-500 text-white px-2 py-1 rounded"
-    >
-      Remove
-    </button>
+    <>
+      <button
+        onClick={() => handleRemove(lat, lon)}
+        className="bg-red-500 text-white px-2 py-1 rounded"
+      >
+        Remove
+      </button>
+      {message && (
+        <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-black/80 text-white px-4 py-2 rounded-xl shadow-lg text-lg font-semibold z-50 ">
+          {message}
+        </div>
+      )}
+    </>
   );
 };
 
