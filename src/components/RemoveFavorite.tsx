4faacddfd6015bdbd5fe10d647a8fde062bd6d4f
przemek <przemek.bialkwno@gmail.com>
2025-11-07 09:21:08 +0100

    synchronizing local storage with context on remove

--- src/components/RemoveFavorite.tsx
+++ src/components/RemoveFavorite.tsx
@@ -13,6 +13,17 @@ const RemoveFavorite = ({ lat, lon }: RemoveFavoriteProps) => {
       type: "REMOVE_FAVORITE",
       payload: { lat, lon },
     });
+
+    const storedList = localStorage.getItem("favorites");
+    const parsedList: RemoveFavoriteProps[] = storedList
+      ? JSON.parse(storedList)
+      : [];
+
+    const updatedList = parsedList.filter(
+      (fav) => !(fav.lat === lat && fav.lon === lon)
+    );
+
+    localStorage.setItem("favorites", JSON.stringify(updatedList));
   };
 
   return <button onClick={handleRemove}>REMOVE</button>;
