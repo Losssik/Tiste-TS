1b37c55934b0b2d4968cffaa2ef0bd40ea29c093
przemek <przemek.bialkwno@gmail.com>
2025-11-20 20:45:50 +0100

    scrapping, setting timeouts

--- server/index.ts
+++ server/index.ts
@@ -10,56 +10,66 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
-const URL = "https://hydro.imgw.pl/#/list/hydro?c=47&rpp=20&pf=0";
+const URL = "https://hydro.imgw.pl/#/list/hydro?c=47&rpp=50&pf=0";
 
 app.use(cors(corsOptions));
 
 app.get("/rivers", async (req: Request, res: Response) => {
   try {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null,
+    });
     const page = await browser.newPage();
     await page.goto(URL);
 
     // wait for slectors
     await page.waitForSelector(".imgw-pop-btn");
 
+    // close cookie popup
     await page.evaluate(() => {
       const buttons = document.querySelectorAll(".imgw-pop-btn");
       buttons.forEach((btn) => (btn as HTMLElement).click());
     });
 
-    // need to wait for selectors always
-    await page.waitForSelector("tr td div > span");
-
-    const cities = await page.$$("tr td div > span");
     const results = [];
 
-    for (const city of cities) {
-      const text = await city.evaluate((el) => el.textContent?.trim());
-      results.push({ stacja: text });
-    }
-
+    // timeout
+    await new Promise((r) => setTimeout(r, 2000));
     await page.waitForSelector("tr");
     const rows = await page.$$("tr");
 
     for (const row of rows) {
+      // station name
+      const station_id = await row.$("td:nth-child(2) div > span");
+      if (!station_id) continue;
+
       // river
       const river_id = await row.$("td:nth-child(3) div");
       if (!river_id) continue;
-      const river = await river_id.evaluate((el) => el.textContent?.trim());
 
       // water level
       const water_id = await row.$("td:nth-child(5) div");
       if (!water_id) continue;
+
+      // trend
+      const trend_id = await row.$("td:nth-child(7) span");
+      if (!trend_id) continue;
+
+      const station = await station_id.evaluate((el) => el.textContent.trim());
+      const river = await river_id.evaluate((el) => el.textContent?.trim());
       const water_level = await water_id.evaluate((el) =>
         el.textContent.trim()
       );
+      const trend = await trend_id.evaluate((el) => el.textContent.trim());
 
-      results.push({ river: river, water_level: water_level });
+      results.push({
+        river: river,
+        water_level: water_level,
+        station: station,
+        trend: trend,
+      });
     }
-
-    // timeout
-    await new Promise((r) => setTimeout(r, 2000));
 
     res.status(200).json({ results });
   } catch (error) {
